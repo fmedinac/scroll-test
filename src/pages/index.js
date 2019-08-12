0d371d8ddcf2edbd7c9417a6fc3071d8646bf1ec
Felipe Medina <medina@work.co>
2019-08-12 18:02:43 -0300

    Set video fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ const IndexPage = () => {
     <Layout isHeaderFixed={isHeaderFixed}>
       <div
         style={{
-          position: 'absolute',
+          position: 'fixed',
           top: 0,
           left: 0,
           right: 0,
